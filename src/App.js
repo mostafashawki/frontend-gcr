10aa566a379d8bfaec09640ded6e11f28ac3c71c
Mostafa Shawki <mostafashawki@gmail.com>
2024-01-01 17:42:29 +0100

    update the backend endpoint

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from 'react';
-const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3003/api/data';
+// const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:3003/api/data';
 // const backendEndpoint = process.env.COPILOT_SERVICE_DISCOVERY_ENDPOINT || 'http://localhost:3000';
 // const apiUrl = `http://fsapp1be.${process.env.COPILOT_SERVICE_DISCOVERY_ENDPOINT}:3000/api/data` || 'http://localhost:3000/api/data';
 // const COPILOT_SERVICE_DISCOVERY_ENDPOINT = `dev.fsapp2.local`
 //http://fsapp2be.${COPILOT_SERVICE_DISCOVERY_ENDPOINT}:3000
-// const apiUrl = 'http://fsapp2be.dev.fsapp2.local:3000/api/data';
+const apiUrl = 'https://backend-gcr-bz4mlgg35a-oe.a.run.app';
 
 function App() {
   const [data, setData] = useState(null);
